0e51c46f6cea424d03c77fa75871b863fd4b0309
Abai Richárd <richarddinozzo@gmail.com>
2014-12-18 22:04:31 +0100

    malvin fájlosítva

---
+++ Malvin/buszmegallo-fajlba.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <osmium/io/any_input.hpp>
+ 
+ 
+bool wayToSort (const std::pair<std::string, long int>& lhs, const std::pair<std::string, long int>& rhs)
+{
+  return lhs.second < rhs.second;
+}
+ 
+int main(int argc, char * argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " input.osmfile" << "\n";
+        exit(1);
+    }
+ 
+    osmium::io::File osmfile(argv[1]);
+    osmium::io::Reader mReader(osmfile, osmium::osm_entity_bits::node);
+ 
+    std::ofstream kimenet ("kifele.txt");
+    if (kimenet.is_open())
+    {
+      std::vector< std::pair<std::string, long int> > nodeokID;
+      std::vector<std::string> megallok;
+ 
+      std::string buszTipus, jaratSzam, megalloNev, elozo;
+      long int megalloID;
+ 
+      while (osmium::memory::Buffer mBuffer = mReader.read())
+      {
+	for(auto& e : mBuffer)
+	{
+	  osmium::Node& node (static_cast<osmium::Node&>(e));
+ 
+	  if(node.tags().get_value_by_key("name"))
+	  {
+	    megalloNev = node.tags().get_value_by_key("name");
+	    megalloID = node.id();
+	    nodeokID.emplace_back(std::make_pair(megalloNev, megalloID));
+	  }
+	}
+      }
+      std::sort(nodeokID.begin(), nodeokID.end(), wayToSort);
+   
+      mReader.close();
+   
+      osmium::io::Reader reader(osmfile, osmium::osm_entity_bits::relation);
+ 
+      while (osmium::memory::Buffer mBuffer = reader.read())
+      {
+	for (auto& it : mBuffer)
+	{
+	  osmium::Relation& mRelation (static_cast<osmium::Relation&>(it));
+ 
+	  if(mRelation.tags().get_value_by_key("route"))
+	  {
+	    buszTipus = mRelation.tags().get_value_by_key("route");
+ 
+	    if(buszTipus == "bus" || buszTipus == "trolleybus")
+	    {
+	      if(mRelation.tags().get_value_by_key("ref"))
+	      {
+		jaratSzam = mRelation.tags().get_value_by_key("ref");
+		kimenet << jaratSzam << " - " << buszTipus << std::endl;
+	      }
+	      else if(mRelation.tags().get_value_by_key("name"))
+	      {
+		jaratSzam = mRelation.tags().get_value_by_key("name");
+		kimenet << jaratSzam << " - " << buszTipus << std::endl;
+	      }
+ 
+	      for( const auto& e : mRelation.members())
+	      {
+		for(auto& i : nodeokID)
+		{
+		  if(i.second == e.ref())
+		  {
+		    if(elozo == i.first)
+		      continue;
+		    else
+		    {
+		      kimenet << "\t" << i.first << std::endl;
+		      elozo = i.first;
+		    }
+		  }
+		}
+	      }
+	    }
+	  }
+	}
+      }
+      
+      google::protobuf::ShutdownProtobufLibrary();
+      reader.close();
+
+      kimenet.close();      
+    }
+    else std::cout << "Nemtudom megnyitni a fájlt";
+ 
+    return 0;
+}
